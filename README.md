ee69cf6dcf1486c8fb9be0ab32469ce1cab02f98
Gilles Renevey <Gilles Renevey>
2016-04-21 21:08:52 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 #Angular 2 Quickstart Webpack version
 
 This is the Angular 2 quickstart from google adapted to work with webpack.
+
+##How to run this quickstart:
+
+>npm install
+
+>npm run build
+
+>npm start
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     entry: "./app/boot",
     output: {
-        // Make sure to use [name] or [id] in output.filename
-        //  when using multiple entry points        
         path: __dirname,
         filename: "./dist/bundle.js"
     },
